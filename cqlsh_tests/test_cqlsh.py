a4c8f5321cff1eca8811d4da22fc6015b53a21ae
Dinesh A. Joshi <dinesh.joshi@apple.com>
2020-02-19 12:09:18 -0800

    fix TestCqlsh::test_describe

--- cqlsh_tests/test_cqlsh.py
+++ cqlsh_tests/test_cqlsh.py
@@ -1001,19 +1001,19 @@ CREATE TYPE test.address_type (
         if self.cluster.version() >= LooseVersion('4.0'):
             create_table += """
         ) WITH CLUSTERING ORDER BY (col ASC)
+            AND additional_write_policy = '99p'
             AND bloom_filter_fp_chance = 0.01
             AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
             AND crc_check_chance = 1.0
-            AND dclocal_read_repair_chance = 0.0
             AND default_time_to_live = 0
             AND gc_grace_seconds = 864000
             AND max_index_interval = 2048
             AND memtable_flush_period_in_ms = 0
             AND min_index_interval = 128
-            AND read_repair_chance = 0.0
+            AND read_repair = 'BLOCKING'
             AND speculative_retry = '99p';
         """
         elif self.cluster.version() >= LooseVersion('3.9'):
@@ -1088,19 +1088,19 @@ CREATE TYPE test.address_type (
             age int,
             firstname text,
             lastname text
-        ) WITH bloom_filter_fp_chance = 0.01
+        ) WITH additional_write_policy = '99p'
+            AND bloom_filter_fp_chance = 0.01
             AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
             AND comment = ''
             AND compaction = {'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32', 'min_threshold': '4'}
             AND compression = {'chunk_length_in_kb': '16', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
             AND crc_check_chance = 1.0
-            AND dclocal_read_repair_chance = 0.0
             AND default_time_to_live = 0
             AND gc_grace_seconds = 864000
             AND max_index_interval = 2048
             AND memtable_flush_period_in_ms = 0
             AND min_index_interval = 128
-            AND read_repair_chance = 0.0
+            AND read_repair = 'BLOCKING'
             AND speculative_retry = '99p';
         """
         elif self.cluster.version() >= LooseVersion('3.9'):
